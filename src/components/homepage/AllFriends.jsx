c702e169dab0509e37dd549aaf0e71aa35f7e0aa
sadik-mahdi <sadikmahadi06@gmail.com>
2026-05-15 22:31:47 +0600

    fixed some div related issue

--- src/components/homepage/AllFriends.jsx
+++ src/components/homepage/AllFriends.jsx
@@ -8,10 +8,10 @@ const Allfriends = () => {
   const friends = use(friendsPromise);
   console.log(friends,'friends');
 
-  return <div className='my-12 container mx-auto'>
+  return <div className='mx-12 container mx-auto'>
       <h2 className='font-bold text-3xl text-left mb-4'>Your Friends</h2>
 
-<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
   {
   friends.map((friend,ind) => {
     return (
